1e2bd59b20fd9ed7e524b26bfad84c3deab3e511
Brandon Still <brandon@agencyrevolution.com>
2015-07-13 16:53:01 -0700

    remove dots and arrows

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,10 +1,11 @@
 $('.slider-main').slick({
   autoplay: true,
   autoplaySpeed: 5000,
-  dots: true,
+  dots: false,
   fade: true,
   infinite: true,
   speed: 300,
+  arrows: 'false',
   slide: 'div',
   cssEase: 'linear'
 });
