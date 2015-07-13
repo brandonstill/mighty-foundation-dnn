7a39bf35b29cdfd7db1fc237cd5bd2845517ec7e
Brandon Still <brandon@agencyrevolution.com>
2015-07-13 16:51:32 -0700

    add quote form validation

--- scripts/reset.js
+++ scripts/reset.js
@@ -14,3 +14,41 @@ $(':input','.field').each(function(index, elem) {
 });
 // remove inline styles from state select
 $('select', '.state').removeAttr( 'style' );
+
+// Disable Dynamic Font Size on Quote Form Headline
+$('h1', '.designation').removeAttr('style').replaceWith(function() {
+	return '<h2>' + $(this).text() + '</h2>';
+});
+
+// Quote Form Required Fields
+$('.quotrr .firstName label').html('First Name*');
+$('.quotrr .lastName label').html('Last Name*');
+$('.quotrr .email label').html('Email*');
+$('.quotrr .phone label').html('Phone*');
+
+// Quote Form Error Handling
+function validize() {
+$('.quotrr .validation').addClass('fillme').html('Please fill out required fields*<br>');
+} 
+
+if ($('.quotrr').length > 0) {
+$('.trigger input').bind('click', function () {  
+  $('.quotrr .validation').html('');  
+  if (!$('.quotrr .firstName .wq-input-field').val()) { 
+    $('.quotrr .firstName .wq-input-field').parent().addClass('fillme');
+    validize();
+  }
+  if (!$('.quotrr .lastName .wq-input-field').val()) { 
+    $('.quotrr .lastName .wq-input-field').parent().addClass('fillme');
+    validize();
+  }
+  if (!$('.quotrr .email .wq-input-field').val()) { 
+    $('.quotrr .email .wq-input-field').parent().addClass('fillme');
+    validize();
+  }
+  if (!$('.quotrr .phone .wq-input-field').val()) { 
+    $('.quotrr .phone .wq-input-field').parent().addClass('fillme');
+    validize();
+  }
+});
+}
