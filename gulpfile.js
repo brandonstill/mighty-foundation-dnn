367f354955219d22e1968a5f8778ad8cb1bae159
Brandon Still <brandon@agencyrevolution.com>
2015-10-13 09:34:17 -0700

    Revert "added eslint, combined tasks, updated file locations"
    This reverts commit d58d7b0d727ae049005f357a5c34c3cad47e0309.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp'),
-    eslint = require('gulp-eslint'),
+    jshint = require('gulp-jshint'),
     browserify = require('gulp-browserify'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
@@ -12,10 +12,65 @@ var gulp = require('gulp'),
 
 // JSHint task
 gulp.task('lint', function() {
-  return gulp.src('js/**/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
+  gulp.src('./scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Browserify task
+gulp.task('browserify', function() {
+  // Single point of entry
+  gulp.src([ './scripts/main.js' ])
+  .pipe(browserify({
+    insertGlobals: true
+  }))
+  // Bundle to a single file
+  .pipe(concat('skin.js'))
+  // Output it to our dist folder
+  .pipe(gulp.dest('./dist/js/'));
+});
+
+// Browserify task
+gulp.task('browserify-min', function() {
+  // Single point of entry
+  gulp.src([ './scripts/main.js' ])
+  .pipe(browserify({
+    insertGlobals: true
+  }))
+  // Bundle to a single file
+  .pipe(concat('skin.js'))
+  // Output it to our dist folder
+  .pipe(gulp.dest('./dist/js/'))
+  .pipe(uglify())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('stylesheets', function() {
+  gulp.src('./styles/*.scss')
+    .pipe(sass({onError: function(e) { console.log(e); },
+      includePaths: [
+        'styles/vendor/foundation/scss',
+        'styles/vendor/utility-belt/scss',
+        'styles/vendor/slick/slick'
+      ]}))
+    .pipe(prefix('last 2 versions'))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('stylesheets-min', function() {
+  gulp.src('./styles/*.scss')
+    .pipe(sass({onError: function(e) { console.log(e); },
+      includePaths: [
+        'styles/vendor/foundation/scss',
+        'styles/vendor/utility-belt/scss',
+        'styles/vendor/slick/slick'
+      ]}))
+    .pipe(prefix('last 2 versions'))
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('minifyJS', function() {
@@ -32,45 +87,32 @@ gulp.task('minifyCSS', function() {
     .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('minify', ['minifyJS', 'minifyCSS']);
+gulp.task('minify', ['minifyJS','minifyCSS']);
 
-// Browserify task
-gulp.task('browserify', function() {
-  // Single point of entry
-  gulp.src([ './js/main.js' ])
-  .pipe(browserify({ insertGlobals: true }))
-  // Bundle to a single file
-  .pipe(concat('skin.js'))
-  // Output it to our dist folder
-  .pipe(gulp.dest('./dist/js/'));
-});
-
-gulp.task('stylesheets', function() {
-  gulp.src('./styles/*.scss')
-    .pipe(sass({onError: function(e) { console.log(e); },
-      includePaths: [
-        'node_modules/foundation-sites/scss',
-        'node_modules/utility-belt/scss',
-        'node_modules/slick-carousel/slick'
-      ]}))
-    .pipe(prefix('last 2 versions'))
-    .pipe(gulp.dest('./dist/css/'));
-});
-
-
-gulp.task('watch', function() {
-  gulp.watch(['./js/**/*.js'], [
+gulp.task('watch', ['lint'], function() {
+  gulp.watch(['./scripts/*.js', './scripts/**/*.js'],[
     'lint',
-    'minifyJS'
+    'browserify'
   ]);
-  gulp.watch(['./styles/**/*.scss'], [
-    'stylesheets',
-    'minifyCSS'
+  gulp.watch(['./styles/*.scss', './styles/**/*.scss'], [
+    'stylesheets'
   ]);
 });
 
-gulp.task('default', ['browserify', 'stylesheets']);
+gulp.task('watch-all', ['lint'], function() {
+  gulp.watch(['./scripts/*.js', './scripts/**/*.js'],[
+    'lint',
+    'browserify-min'
+  ]);
+  gulp.watch(['./styles/*.scss', './styles/**/*.scss'], [
+    'stylesheets-min'
+  ]);
+});
 
-gulp.task('dev', ['default', 'watch']);
+gulp.task('default', ['browserify','stylesheets']);
 
-gulp.task('build', ['default', 'minify']);
+gulp.task('dev', ['default','watch']);
+
+gulp.task('build', ['default','minify']);
+
+gulp.task('build-watch', ['default','watch-all']);
